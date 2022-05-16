8b41875e43d6ebbdb20ba22e7989a07d41f6564d
josevictormoreno <josevictor.admoreno@gmail.com>
2022-05-16 10:16:42 -0300

    feat: adds PySimpleGUI and creates globais

--- script/main.py
+++ script/main.py
@@ -1,2 +1,23 @@
-import pytube
+from pytube import YouTube
+import globais as g
+import PySimpleGUI as sg
 
+sg.theme('Material1')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Paste video link here...')],
+            [sg.Text('URL: '), sg.InputText()],
+            [sg.Button('Download'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('Youtube Video Downloader', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    video_link =  values[0]
+
+    video = YouTube(video_link)
+    video.streams.filter(res='1080').first().download('/home/nriskdev2/Downloads')
+
+window.close()
