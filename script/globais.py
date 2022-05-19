5f9dd7c3a7b1528a4f9eed58f117c480e689e418
josevictormoreno <josevictor.admoreno@gmail.com>
2022-05-19 20:37:28 -0300

    commit final

--- script/globais.py
+++
@@ -1,4 +0,0 @@
-width = 100
-height = 100
-font_color = 'white'
-background_color = 'black'
--- script/main.py
+++ script/main.py
@@ -1,26 +1,26 @@
 from pytube import YouTube
-import globais as g
 import PySimpleGUI as sg
 
-sg.theme('Material1')  # theme of the interface
-# All the stuff inside your window.
+#########################################
+#      github: https://github.com/josevictormoreno      #
+#########################################
+
+sg.theme('Material1') 
 layout = [[sg.Text('Paste video link here...')],
           [sg.Text('URL: '), sg.InputText()],
-          [sg.Button('Download'), sg.Button('Cancel')]]
+          [sg.Button('Download', key='download'), sg.Button('Cancel')],
+          [sg.Output(size=(40,2))]]
 
-# Create the Window
 window = sg.Window('Youtube Video Downloader', layout)
-# Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel': 
         break
     video_link = values[0]
-    print('url digitada: ' + video_link)
-
     video = YouTube(video_link)
 
     stream = video.streams.get_highest_resolution()
     stream.download(output_path='./downloads')
+    print('Download Complete!')
 
 window.close()
