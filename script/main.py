31dfa05a55ed4e237bf3723745a3c25cef77e221
josevictormoreno <josevictor.admoreno@gmail.com>
2022-05-20 20:32:26 -0300

    feat: includes mpd downloads

--- script/main.py
+++ script/main.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import PySimpleGUI as sg
+import os
 
 #########################################
 #      github: https://github.com/josevictormoreno      #
@@ -8,6 +9,7 @@ import PySimpleGUI as sg
 sg.theme('Material1') 
 layout = [[sg.Text('Paste video link here...')],
           [sg.Text('URL: '), sg.InputText()],
+          [sg.Checkbox('MP3', default=False, key='mp3')],
           [sg.Button('Download', key='download'), sg.Button('Cancel')],
           [sg.Output(size=(40,2))]]
 
@@ -18,9 +20,16 @@ while True:
         break
     video_link = values[0]
     video = YouTube(video_link)
-
-    stream = video.streams.get_highest_resolution()
-    stream.download(output_path='./downloads')
+    mp3 = values['mp3']
+    if mp3 == True:
+        stream = video.streams.filter(only_audio=True).first()
+        out_file = stream.download(output_path='./downloaded')
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+    else:
+        stream = video.streams.get_highest_resolution()
+        stream.download(output_path='./downloaded')
     print('Download Complete!')
 
 window.close()
